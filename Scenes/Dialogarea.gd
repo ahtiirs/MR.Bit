43e6474f1b979063fd2f2858aec30be09292c9f7
ahtiirs <78646730+ahtiirs@users.noreply.github.com>
2022-06-15 08:59:33 +0300

    Beta V1.5 parandused(veel pooleli)

--- Scenes/Dialogarea.gd
+++ Scenes/Dialogarea.gd
@@ -3,14 +3,22 @@ extends Polygon2D
 
 # Declare member variables here. Examples:
 # var a = 2
-# var b = "text"
+onready var timeout = 1
 
 signal dialog_start
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
+	
+func _process(delta):
+	if self.visible:
 
+		if Input.get_action_strength("ok") || Input.get_action_strength("ok") && timeout <= 0:
+			timeout=0.7
+			$Dialog._on_Button_pressed()
+		timeout -= delta	
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
--- Scenes/StaapText.gd
+++ Scenes/StaapText.gd
@@ -1,6 +1,6 @@
 extends WindowDialog
 
-
+onready var staap = get_parent()
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -9,7 +9,12 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
-
+func _process(delta):
+	if self.visible:
+		if Input.get_action_strength("esc"):
+			staap._on_ok_pressed()
+		if Input.get_action_strength("ok"):
+			staap._on_ok_pressed()
 
 func _on_ok_pressed():
 	self.visible = false 
